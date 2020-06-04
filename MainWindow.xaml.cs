b5f220afdb16868c3e170181f70e556303003c03
Elijah Posnikov <elijah.posnikov@outlook.com>
2020-06-04 14:37:06 +0100

    Added Colour functionality to signify correct/incorrect answer

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -46,8 +46,12 @@ namespace Typer
             {
                 for (int i = 0; i < 50; i++)
                 {
+                    //generate random index from 0-1000
                     int r = rnd.Next(textFieldArr.Length);
-                    tempText += textFieldArr[r] + " ";
+
+                    //if temptext does not already contain word, add to temptext
+                    if (!tempText.Contains(textFieldArr[r])) tempText += textFieldArr[r] + " ";
+    
                 }
                 SetGetText.SetText(mainText, tempText);
             }
@@ -64,11 +68,39 @@ namespace Typer
         private void RfrshBtn_Click(object sender, RoutedEventArgs e)
         {
             Refresh();
-        }       
+        }
+
+        //method to color specific text in richtextbox
+        public static void FromTextPointer(TextPointer startPointer, TextPointer endPointer, string keyword, Brush foreground)
+        {
+            if (startPointer == null) throw new ArgumentNullException(nameof(startPointer));
+            if (endPointer == null) throw new ArgumentNullException(nameof(endPointer));
+            if (string.IsNullOrEmpty(keyword)) throw new ArgumentNullException(keyword);
+
+            TextRange text = new TextRange(startPointer, endPointer);
+            TextPointer current = text.Start.GetInsertionPosition(LogicalDirection.Forward);
+            while (current != null)
+            {
+                string textInRun = current.GetTextInRun(LogicalDirection.Forward);
+                if (!string.IsNullOrWhiteSpace(textInRun))
+                {
+                    int index = textInRun.IndexOf(keyword);
+                    if (index != -1)
+                    {
+                        TextPointer selectionStart = current.GetPositionAtOffset(index, LogicalDirection.Forward);
+                        TextPointer selectionEnd = selectionStart.GetPositionAtOffset(keyword.Length, LogicalDirection.Forward);
+                        TextRange selection = new TextRange(selectionStart, selectionEnd);
+
+                        selection.ApplyPropertyValue(TextElement.ForegroundProperty, foreground);
+                    }
+                }
+                current = current.GetNextContextPosition(LogicalDirection.Forward);
+            }
+        }
 
         private void EntryBox_KeyDown(object sender, KeyEventArgs e)
         {
-            
+
             if (e.Key == Key.Space)
             {
 
@@ -91,14 +123,18 @@ namespace Typer
 
                         //prevent user entry when words have ran out
                         entryBox.IsEnabled = false;
-                        System.Diagnostics.Debug.WriteLine("Timer stopped");
+                        Debug.WriteLine("Timer stopped");
                     }
 
                     //check if user input matches current word in array
                     if (userInputWord == checkInputArr[currentWordIndex])
                     {
                         correctWords += 1;
-                    } 
+                        FromTextPointer(mainText.Document.ContentStart, mainText.Document.ContentEnd, checkInputArr[currentWordIndex] + " ", Brushes.Green);
+                    } else
+                    {
+                        FromTextPointer(mainText.Document.ContentStart, mainText.Document.ContentEnd, checkInputArr[currentWordIndex] + " ", Brushes.Red);
+                    }
 
                     try
                     {
@@ -134,9 +170,24 @@ namespace Typer
                         accNum.Foreground = new SolidColorBrush(System.Windows.Media.Color.FromRgb(194, 0, 32));
                     }
 
-                    //set label to accuracy and wpm num
-                    wpmNum.Content = Math.Round(wpm, 0);
-                    accNum.Content = Math.Round(accuracy, 0) + "%";
+                    //set label to accuracy and wpm
+                    //make labels a bit nicer, instead of displaying NaN, displays "N/A"
+                    if (Double.IsNaN(wpm))
+                    {
+                        wpmNum.Content = "N/A";
+                    } else
+                    {
+                        wpmNum.Content = Math.Round(wpm, 0);
+                    }
+
+                    if (Double.IsNaN(accuracy))
+                    {
+                        accNum.Content = "N/A";
+                    } else
+                    {
+                        accNum.Content = Math.Round(accuracy, 0) + "%";
+                    }
+                    
                 } 
 
             }
@@ -160,5 +211,32 @@ namespace Typer
         public double accuracy = 0;
         public Stopwatch sw = new Stopwatch();
         public double wpm = 0;
+
+        private void ThemeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Refresh();
+            accLabel.Visibility = Visibility.Hidden;
+            accNum.Visibility = Visibility.Hidden;
+            wpmLabel.Visibility = Visibility.Hidden;
+            wpmNum.Visibility = Visibility.Hidden;
+            RfrshBtn.Visibility = Visibility.Hidden;
+            entryBox.Visibility = Visibility.Hidden;
+            entryRect.Visibility = Visibility.Hidden;
+            mainText.Visibility = Visibility.Hidden;
+            saveThemeBtn.Visibility = Visibility.Visible;
+        }
+
+        private void saveThemeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            accLabel.Visibility = Visibility.Visible;
+            accNum.Visibility = Visibility.Visible;
+            wpmLabel.Visibility = Visibility.Visible;
+            wpmNum.Visibility = Visibility.Visible;
+            RfrshBtn.Visibility = Visibility.Visible;
+            entryBox.Visibility = Visibility.Visible;
+            entryRect.Visibility = Visibility.Visible;
+            mainText.Visibility = Visibility.Visible;
+            saveThemeBtn.Visibility = Visibility.Hidden;
+        }
     }
 }

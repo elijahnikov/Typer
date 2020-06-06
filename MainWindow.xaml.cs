783bbd7b2bf01d2175a65dda93b34c383542c7b9
Elijah Posnikov <elijah.posnikov@outlook.com>
2020-06-07 00:02:02 +0100

    Version update
    - Added four new themes
    - Added colours to signify correct, incorrect and next words

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,6 +4,15 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Diagnostics;
 using System.Windows.Documents;
+using System.Windows.Controls;
+using System.Configuration;
+using System.IO;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text.RegularExpressions;
+using OpenTK.Graphics.ES10;
+using System.Security.Policy;
 
 namespace Typer
 {
@@ -12,13 +21,39 @@ namespace Typer
         public MainWindow()
         {  
             InitializeComponent();
-            BrushConverter bc = new BrushConverter();
-            MainGrid.Background = (Brush)bc.ConvertFrom("#FAFAFA");
-            textFieldArr = Properties.Resources.TextFile.Split(" ");
+            textFieldArr = Properties.Resources.EnglishText.Split(" ");
             SetTextBox();
             entryBox.Focus();
+
+            if (!File.Exists(themePath))
+            {
+                File.Create(themePath).Close();
+            }
+
+            if (new FileInfo(themePath).Length != 0)
+            {
+                //applies saved theme on startup
+                string line = File.ReadLines(themePath).First();
+                switch (line)
+                {
+                    case "Olivia":
+                        Olivia();
+                        break;
+                    case "Dracula":
+                        Dracula();
+                        break;
+                    case "Dolche":
+                        Dolche();
+                        break;
+                    case "Ashes":
+                        Ashes();
+                        break;
+                    case "":
+                        break;
+                }
+            }
         }
-        private void Refresh()
+        public void Refresh()
         {
             tempText = "";
             entryBox.IsEnabled = true;
@@ -61,8 +96,7 @@ namespace Typer
             }
 
             String getMainText = SetGetText.GetText(mainText);
-            checkInputArr = getMainText.Split(" ");
-
+            checkInputArr = getMainText.Split(" ");  
         }
 
         private void RfrshBtn_Click(object sender, RoutedEventArgs e)
@@ -71,31 +105,58 @@ namespace Typer
         }
 
         //method to color specific text in richtextbox
-        public static void FromTextPointer(TextPointer startPointer, TextPointer endPointer, string keyword, Brush foreground)
+        public static void FromTextPointer(TextPointer startPointer, TextPointer endPointer, string keyword, String foreground)
         {
             if (startPointer == null) throw new ArgumentNullException(nameof(startPointer));
             if (endPointer == null) throw new ArgumentNullException(nameof(endPointer));
             if (string.IsNullOrEmpty(keyword)) throw new ArgumentNullException(keyword);
 
-            TextRange text = new TextRange(startPointer, endPointer);
-            TextPointer current = text.Start.GetInsertionPosition(LogicalDirection.Forward);
-            while (current != null)
-            {
-                string textInRun = current.GetTextInRun(LogicalDirection.Forward);
-                if (!string.IsNullOrWhiteSpace(textInRun))
-                {
-                    int index = textInRun.IndexOf(keyword);
-                    if (index != -1)
-                    {
-                        TextPointer selectionStart = current.GetPositionAtOffset(index, LogicalDirection.Forward);
-                        TextPointer selectionEnd = selectionStart.GetPositionAtOffset(keyword.Length, LogicalDirection.Forward);
-                        TextRange selection = new TextRange(selectionStart, selectionEnd);
+            List<string> list = new List<string>();
 
-                        selection.ApplyPropertyValue(TextElement.ForegroundProperty, foreground);
+            if (!list.Contains(keyword))
+            {
+                TextRange text = new TextRange(startPointer, endPointer);
+                TextPointer current = text.Start.GetInsertionPosition(LogicalDirection.Forward);
+                while (current != null)
+                {
+                    string textInRun = current.GetTextInRun(LogicalDirection.Forward);
+                    if (!string.IsNullOrWhiteSpace(textInRun))
+                    {
+                        int index = textInRun.IndexOf(keyword);
+                        if (index != -1)
+                        {
+                            TextPointer selectionStart = current.GetPositionAtOffset(index, LogicalDirection.Forward);
+                            TextPointer selectionEnd = selectionStart.GetPositionAtOffset(keyword.Length, LogicalDirection.Forward);
+                            TextRange selection = new TextRange(selectionStart, selectionEnd);
+                            BrushConverter bc = new BrushConverter();
+                            Brush brush = (Brush)bc.ConvertFrom(foreground);
+                            selection.ApplyPropertyValue(TextElement.ForegroundProperty, brush);
+                        }
                     }
+                    current = current.GetNextContextPosition(LogicalDirection.Forward);
                 }
-                current = current.GetNextContextPosition(LogicalDirection.Forward);
             }
+
+            list.Add(keyword);
+        }
+
+        //method to append current theme to text file, used to save theme on close and reopen
+        private void appendTheme(String path, String text)
+        {
+            var allLines = File.ReadAllLines(path);
+
+            if (allLines.Length > 0)
+            {
+                try
+                {
+                    allLines[0] = text;
+                    File.WriteAllLines(path, allLines);
+                }
+                catch (IndexOutOfRangeException ex) { }
+            } else
+            {
+                File.WriteAllText(path, text);
+            } 
         }
 
         private void EntryBox_KeyDown(object sender, KeyEventArgs e)
@@ -103,6 +164,16 @@ namespace Typer
 
             if (e.Key == Key.Space)
             {
+
+                //current bug: color system will recolor all instances of a word even if string in string
+                //to combat this, loop to continuously recolor the rest of the text not attempted by user
+                //should find a better way to fix this, not priority right now
+                for (int i = currentWordIndex + 1; i < checkInputArr.Length; i++)
+                {
+                    FromTextPointer(mainText.Document.ContentStart, mainText.Document.ContentEnd, checkInputArr[i] + " ", chosenColor);
+                }
+
+                FromTextPointer(mainText.Document.ContentStart, mainText.Document.ContentEnd, checkInputArr[currentWordIndex + 1] + " ", nextColor);
 
                 if (!string.IsNullOrWhiteSpace(entryBox.Text))
                 {
@@ -120,7 +191,6 @@ namespace Typer
                     if (currentWordIndex == 49)
                     {
                         sw.Stop();
-
                         //prevent user entry when words have ran out
                         entryBox.IsEnabled = false;
                         Debug.WriteLine("Timer stopped");
@@ -130,12 +200,13 @@ namespace Typer
                     if (userInputWord == checkInputArr[currentWordIndex])
                     {
                         correctWords += 1;
-                        FromTextPointer(mainText.Document.ContentStart, mainText.Document.ContentEnd, checkInputArr[currentWordIndex] + " ", Brushes.Green);
+                        FromTextPointer(mainText.Document.ContentStart, mainText.Document.ContentEnd, checkInputArr[currentWordIndex] + " ", correctColor);
                     } else
                     {
-                        FromTextPointer(mainText.Document.ContentStart, mainText.Document.ContentEnd, checkInputArr[currentWordIndex] + " ", Brushes.Red);
+                        FromTextPointer(mainText.Document.ContentStart, mainText.Document.ContentEnd, checkInputArr[currentWordIndex] + " ", incorrectColor);
                     }
 
+                    //calculate words per minute
                     try
                     {
                         long seconds = sw.ElapsedMilliseconds / 1000;
@@ -158,40 +229,15 @@ namespace Typer
                     //increment index to next word in array
                     currentWordIndex += 1;
 
-                    //set colour of accuracy number
-                    if (accuracy >= 75)
-                    {
-                        accNum.Foreground = new SolidColorBrush(System.Windows.Media.Color.FromRgb(0, 176, 15));
-                    } else if (accuracy < 75 && accuracy > 40)
-                    {
-                        accNum.Foreground = new SolidColorBrush(System.Windows.Media.Color.FromRgb(194, 187, 0));
-                    } else
-                    {
-                        accNum.Foreground = new SolidColorBrush(System.Windows.Media.Color.FromRgb(194, 0, 32));
-                    }
-
                     //set label to accuracy and wpm
                     //make labels a bit nicer, instead of displaying NaN, displays "N/A"
-                    if (Double.IsNaN(wpm))
-                    {
-                        wpmNum.Content = "N/A";
-                    } else
-                    {
-                        wpmNum.Content = Math.Round(wpm, 0);
-                    }
+                    if (Double.IsNaN(wpm)) {wpmNum.Content = "N/A";} 
+                    else {wpmNum.Content = Math.Round(wpm, 0);}
 
-                    if (Double.IsNaN(accuracy))
-                    {
-                        accNum.Content = "N/A";
-                    } else
-                    {
-                        accNum.Content = Math.Round(accuracy, 0) + "%";
-                    }
-                    
+                    if (Double.IsNaN(accuracy)) {accNum.Content = "N/A";} 
+                    else {accNum.Content = Math.Round(accuracy, 0) + "%";} 
                 } 
-
             }
-
         }
 
         private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
@@ -201,16 +247,6 @@ namespace Typer
                 Refresh();
             }
         }
-
-        //declaring variables
-        public String[] textFieldArr;
-        public String[] checkInputArr;
-        public String tempText = "";
-        public int currentWordIndex = 0;
-        public int correctWords = 0;
-        public double accuracy = 0;
-        public Stopwatch sw = new Stopwatch();
-        public double wpm = 0;
 
         private void ThemeBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -224,6 +260,11 @@ namespace Typer
             entryRect.Visibility = Visibility.Hidden;
             mainText.Visibility = Visibility.Hidden;
             saveThemeBtn.Visibility = Visibility.Visible;
+
+            OliviaThemeBtn.Visibility = Visibility.Visible;
+            DraculaThemeBtn.Visibility = Visibility.Visible;
+            ModernDolchThemeBtn.Visibility = Visibility.Visible;
+            AshesThemeBtn.Visibility = Visibility.Visible;
         }
 
         private void saveThemeBtn_Click(object sender, RoutedEventArgs e)
@@ -237,6 +278,135 @@ namespace Typer
             entryRect.Visibility = Visibility.Visible;
             mainText.Visibility = Visibility.Visible;
             saveThemeBtn.Visibility = Visibility.Hidden;
+
+            OliviaThemeBtn.Visibility = Visibility.Hidden;
+            DraculaThemeBtn.Visibility = Visibility.Hidden;
+            ModernDolchThemeBtn.Visibility = Visibility.Hidden;
+            AshesThemeBtn.Visibility = Visibility.Hidden;
         }
+
+        private void OliviaThemeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Olivia();
+            File.WriteAllText("Olivia", "Theme.txt");
+        }
+
+        private void DraculaThemeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Dracula();
+        }
+
+        private void ModernDolchThemeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Dolche();
+        }
+
+        private void AshesThemeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Ashes();
+        }
+
+        private void Olivia()
+        {
+            BrushConverter bc = new BrushConverter();
+            MainGrid.Background = (Brush)bc.ConvertFrom("#302F2D");
+            mainRect.Background = (Brush)bc.ConvertFrom("#D5D5CD");
+            RfrshBtn.Background = (Brush)bc.ConvertFrom("#302F2D");
+            RfrshBtn.Foreground = (Brush)bc.ConvertFrom("#DAAB99");
+            ThemeBtn.Background = (Brush)bc.ConvertFrom("#3b3937");
+            ThemeBtn.Foreground = (Brush)bc.ConvertFrom("#DAAB99");
+            accNum.Foreground = (Brush)bc.ConvertFrom("#DAAB99");
+            accLabel.Foreground = (Brush)bc.ConvertFrom("#DAAB99");
+            wpmNum.Foreground = (Brush)bc.ConvertFrom("#DAAB99");
+            wpmLabel.Foreground = (Brush)bc.ConvertFrom("#DAAB99");
+            mainText.Foreground = (Brush)bc.ConvertFrom("#302F2D");
+            entryBox.Foreground = (Brush)bc.ConvertFrom("#DAAB99");
+            entryRect.Fill = (Brush)bc.ConvertFrom("#302F2D");
+            correctColor = "#00b32a";
+            chosenColor = "#302F2D";
+            appendTheme(themePath, "Olivia");
+        }
+
+        private void Dracula()
+        {
+            BrushConverter bc = new BrushConverter();
+            MainGrid.Background = (Brush)bc.ConvertFrom("#3B3F50");
+            mainRect.Background = (Brush)bc.ConvertFrom("#23222E");
+            RfrshBtn.Background = (Brush)bc.ConvertFrom("#383C4D");
+            RfrshBtn.Foreground = (Brush)bc.ConvertFrom("#F7FAFD");
+            ThemeBtn.Background = (Brush)bc.ConvertFrom("#23222E");
+            ThemeBtn.Foreground = (Brush)bc.ConvertFrom("#F7FAFD");
+            accNum.Foreground = (Brush)bc.ConvertFrom("#A790D6");
+            accLabel.Foreground = (Brush)bc.ConvertFrom("#D88EC7");
+            wpmNum.Foreground = (Brush)bc.ConvertFrom("#69E199");
+            wpmLabel.Foreground = (Brush)bc.ConvertFrom("#F2FEA3");
+            mainText.Foreground = (Brush)bc.ConvertFrom("#F2F2F5");
+            entryBox.Foreground = (Brush)bc.ConvertFrom("#B9A0E8");
+            entryRect.Fill = (Brush)bc.ConvertFrom("#393D4E");
+            correctColor = "#00b32a";
+            chosenColor = "#F2F2F5";
+            appendTheme(themePath, "Dracula");
+        }
+
+        private void Dolche()
+        {
+            BrushConverter bc = new BrushConverter();
+            MainGrid.Background = (Brush)bc.ConvertFrom("#7e8488");
+            mainRect.Background = (Brush)bc.ConvertFrom("#5a5d64");
+            RfrshBtn.Background = (Brush)bc.ConvertFrom("#69d6d1");
+            RfrshBtn.Foreground = (Brush)bc.ConvertFrom("#ffffff");
+            ThemeBtn.Background = (Brush)bc.ConvertFrom("#69d6d1");
+            ThemeBtn.Foreground = (Brush)bc.ConvertFrom("#ffffff");
+            accNum.Foreground = (Brush)bc.ConvertFrom("#ffffff");
+            accLabel.Foreground = (Brush)bc.ConvertFrom("#ffffff");
+            wpmNum.Foreground = (Brush)bc.ConvertFrom("#ffffff");
+            wpmLabel.Foreground = (Brush)bc.ConvertFrom("#ffffff");
+            mainText.Foreground = (Brush)bc.ConvertFrom("#ffffff");
+            entryBox.Foreground = (Brush)bc.ConvertFrom("#000000");
+            entryRect.Fill = (Brush)bc.ConvertFrom("#ffffff");
+            correctColor = "#69d6d1";
+            incorrectColor = "#ff3b3b";
+            chosenColor = "#ffffff";
+            appendTheme(themePath, "Dolche");
+        }
+
+        private void Ashes()
+        {
+            BrushConverter bc = new BrushConverter();
+            MainGrid.Background = (Brush)bc.ConvertFrom("#d1dadf");
+            mainRect.Background = (Brush)bc.ConvertFrom("#62686e");
+            RfrshBtn.Background = (Brush)bc.ConvertFrom("#3a3b3d");
+            RfrshBtn.Foreground = (Brush)bc.ConvertFrom("#d3dae0");
+            ThemeBtn.Background = (Brush)bc.ConvertFrom("#3a3b3d");
+            ThemeBtn.Foreground = (Brush)bc.ConvertFrom("#ffffff");
+            accNum.Foreground = (Brush)bc.ConvertFrom("#64696F");
+            accLabel.Foreground = (Brush)bc.ConvertFrom("#64696F");
+            wpmNum.Foreground = (Brush)bc.ConvertFrom("#64696F");
+            wpmLabel.Foreground = (Brush)bc.ConvertFrom("#64696F");
+            mainText.Foreground = (Brush)bc.ConvertFrom("#d3dae0");
+            entryBox.Foreground = (Brush)bc.ConvertFrom("#d3dae0");
+            entryRect.Fill = (Brush)bc.ConvertFrom("#3a3b3d");
+            correctColor = "#00b32a";
+            chosenColor = "#d3dae0";
+            appendTheme(themePath, "Ashes");
+        }
+
+        //declaring variables
+        public String[] textFieldArr;
+        public String[] checkInputArr;
+        public String[] recolorText = new string[50];
+        public String tempText = "";
+        public List list;
+        public int currentWordIndex = 0;
+        public int correctWords = 0;
+        public double accuracy = 0;
+        public Stopwatch sw = new Stopwatch();
+        public double wpm = 0;
+        public static BrushConverter bc = new BrushConverter();
+        public String chosenColor = "#000000";
+        public String correctColor = "#00b32a";
+        public String incorrectColor = "#ff0000";
+        public String nextColor = "#c848fa";
+        public String themePath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Theme.txt";
     }
 }
